f2a058ebd4c70e3199fd7fd877fee416c698b4f1
afshin imani zadeh <a.imanizadeh@outlook.com>
2020-09-26 17:39:27 +0330

    singleton

---
+++ GangofFour/CreationalDesingPatterns/Singleton/UserState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GangofFour.CreationalDesingPatterns.Singleton
+{
+    public sealed class UserState
+    {
+        private static UserState _instance;
+        private static object _lock = new object();
+
+        private UserState() { }
+
+
+        public static UserState GetSingleton()
+        {
+
+            lock (_lock)
+            {
+                if (_instance is null)
+                    _instance = new UserState();
+            }
+
+            return _instance;
+        }
+
+
+        public string LoginName { get; set; }
+
+        public short Age { get; set; }
+    }
+}
---
+++ GangofFour/Program.cs
@@ -0,0 +1,33 @@
+﻿using GangofFour.CreationalDesingPatterns.Singleton;
+using System;
+
+namespace GangofFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Singleton
+
+            UserState user = UserState.GetSingleton();
+            user.LoginName = "imanizadeh.me";
+            user.Age = 28;
+
+
+            UserState user2 = UserState.GetSingleton();
+            Console.WriteLine(user2.LoginName);
+            Console.WriteLine(user2.Age);
+            Console.WriteLine(user == user2); 
+
+            #endregion
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
