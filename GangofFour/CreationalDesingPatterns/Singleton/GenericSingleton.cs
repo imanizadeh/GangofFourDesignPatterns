e317caca3f54f85baeb142527be65951e7e99416
afshin imani zadeh <a.imanizadeh@outlook.com>
2020-09-27 23:45:19 +0330

    add generic singleton class

---
+++ GangofFour/CreationalDesingPatterns/Singleton/GenericSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GangofFour.CreationalDesingPatterns.Singleton
+{
+    public sealed class GenericSingleton<T> where T : class, new()
+    {
+        private static T _singletoneInstance;
+        private static object _lock = new object();
+
+        public static T GetSingleton()
+        {
+            //این شرط باعث می شود اگر نمونه کلاس قبلا ایجاد شده است دیگر نخ ها بلاک نشوند
+            if (_singletoneInstance == null)
+            {
+                lock (_lock)
+                {
+                    if (_singletoneInstance == null)
+                        _singletoneInstance = new T();
+                }
+            }
+            return _singletoneInstance;
+        }
+    }
+}
